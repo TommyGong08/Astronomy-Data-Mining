84ae794f8553e5c053960ae380bce7d6300edd5a
TommgGong08 <1034231958@qq.com>
2021-04-22 23:08:21 +0800

    add predict

--- src/LightGBM.py
+++ src/LightGBM.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+from sklearn.model_selection import KFold
 from pandas.core.frame import DataFrame
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -9,8 +10,10 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.feature_selection import SelectKBest
+from sklearn.metrics import f1_score
 import lightgbm as lgb
 
+'''
 # 制作X_train
 train_data_df = pd.read_csv("attribute.csv")
 train_data_df.drop(['Unnamed: 0'], inplace=True, axis=1)
@@ -27,10 +30,30 @@ y_train_df.loc[y_train_df['type'] == 'unknown'] = 3
 y_train_df = y_train_df['type']
 Y_train = y_train_df.values
 print(Y_train)
+'''
+
+# 开始预测
+# 将label.csv和attributes合并 之后划分验证集交叉验证
+test_data = pd.read_csv("test_data.csv")
+test_data.drop(['Unnamed: 0'], inplace=True, axis=1)
+print(test_data)
+test_data_np = test_data.values
+print(test_data_np)
+
+X_test_data = test_data_np[:, 0:2600]
+Y_test_data = test_data_np[:, 2600]
+
+# K-Fold方法划分数据集和验证集
+kf = KFold(n_splits=10)
+for train_index, test_index in kf.split(X_test_data):
+    print('train_index', train_index, 'test_index', test_index)
+    train_X, train_y = X_test_data[train_index], Y_test_data[train_index]
+    test_X, test_y = X_test_data[test_index], Y_test_data[test_index]
 
 # 构造lgb
-lgb_train = lgb.Dataset(X_train, label=Y_train)
-'''params = {'task': 'train',
+lgb_train = lgb.Dataset(train_X, label=train_y)
+'''
+params = {'task': 'train',
                'boosting': 'gbdt',
                'application': 'multiclass',
                'num_class': 4,
@@ -43,7 +66,8 @@ lgb_train = lgb.Dataset(X_train, label=Y_train)
                  'bagging_freq': 2,
                'bagging_seed': 5048,
                'feature_fraction_seed': 2048
-}'''
+}
+'''
 params = {
     'boosting_type': 'gbdt',
     'objective': 'multiclass',
@@ -69,4 +93,16 @@ bst = lgb.train(params,  lgb_train,  num_boost_round=10000)
 print('finished')
 
 # 开始预测
+predict_y = bst.predict(test_X)
+predict_y = predict_y.tolist()
+print(predict_y)
+predict_Y = []
+for i in range(1000):
+    # print(predict_y[i])
+    num = predict_y[i].index(max(predict_y[i]))
+    # print(num)
+    predict_Y.append(num)
+print(predict_Y)
 
+score = f1_score(test_y, predict_Y, average = 'macro')
+print("score: %f" % score)
